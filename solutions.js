c7f802e0ef46b29638364ce5b97266ddec46a773
Salah Ismail <salah.o.ismail01@gmail.com>
2020-10-07 20:41:02 -0500

    practice test

--- solutions.js
+++ solutions.js
@@ -14,35 +14,63 @@
 
 //TODO: Define a function named isOdd that takes in any input and returns true if the input is an odd number, return
 // false for any other situation. You do not need to consider floating-point and decimal numbers.
+function isOdd(arg) {
+    return arg % 2 === 1
+}
 
 
 //TODO: Define a function named isVowel that takes in a single character and returns true if the character is a vowel.
 // *Not including the letter 'y'
-
-
+function isVowel(arg) {
+    if (arg === "a" || arg === "e" || arg === "i" || arg === "o" || arg === "u" || arg === "i" || arg === "A" || arg === "E" || arg === "I" || arg === "O" || arg === "U") {
+        return true
+    }else {return false}
+}
 //TODO: Define a function named combineStrings that takes in two parameters, if both parameters are strings,
 // then return a concatenated string of both parameters. Otherwise, return false.
-
-
+function combineStrings(arg1,arg2) {
+    if(typeof arg1 === "string" && typeof arg2 === "string") {
+        return arg1 + arg2
+    }
+    return false
+}
 //TODO: Define a function named sumArgs that takes in three parameters, if they are numbers then add them together.
 // Otherwise return false.
-
-
+function sumArgs(num1,num2,num3) {
+    if (typeof num1 === "number" && typeof num2 === "number" && typeof num3 === "number"){
+        return num1 + num2 + num3
+    }
+    return false;
+}
 
 //TODO: Define a function named truncateString that takes in a string as the first parameter and a number as the
 // second parameter. Truncate (shorten) the string by the length based on the argument passed in.
 // For example, truncateString("hello world",1) would return "hello worl"
+function truncateString(str,num){
+    if (typeof str === "string" && typeof num === "number")
+    {
+
+    }
+}
 
 
 //TODO: Define a function named `reverseSign` that takes in a number as the first parameter.
 // Reverse the sign of the argument passed in, return false if the argument is not a number.
 // For example, `reverseSign(1)` would return `-1`.
-
+function reverseSign(arg){
+    if (Number(arg)){
+        return arg * -1
+    }    else if (isNaN(arg)){
+        return false
+    }
+    }
 
 //TODO: Define a function named `makeUppercase` that takes in a string as the first parameter.
 // Return the argument passed in entirely uppercased, return false if the argument is not a string.
 // For example, `makeUppercase("test")` would return `"TEST"`.
-
+function makeUppercase(arg) {
+    return (arg === "string" && arg === "string".toUpperCase("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z") )
+}
 
 //TODO: Define a function named `isMultiple` that takes in two `number` parameters.
 // Return `true` if one argument is a multiple of the other, `false` otherwise. For example, `isMultiple(2, 6)`
